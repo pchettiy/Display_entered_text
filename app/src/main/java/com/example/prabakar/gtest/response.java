19c1195afe9fbc8360ec60445413975eabb3785a
Prabakar <praba1110@gmail.com>
2015-03-21 21:25:51 +0530

    This is an app that displays whatever you type.
    I am in a learning stage,this is sort of my first app.

---
+++ app/src/main/java/com/example/prabakar/gtest/response.java
@@ -0,0 +1,28 @@
+package com.example.prabakar.gtest;
+
+import android.content.SharedPreferences;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+/**
+ * Created by Prabakar on 20-03-2015.
+ */
+public class response implements OnClickListener {
+    Gactivity caller;
+
+   public String s;
+   public response (Gactivity activity)
+    {this.caller=activity;
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+
+
+    }
+}
